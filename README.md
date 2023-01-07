b8dc23c58030f4dc0867e756ccca71f7b998321c
Sujay Kadam <sujay.kadam17@siesgst.ac.in>
2023-01-07 23:19:06 +0530

    Base Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# blog-demo
+
+## Build Setup
+
+```bash
+# install dependencies
+$ yarn install
+
+# serve with hot reload at localhost:3000
+$ yarn dev
+
+# build for production and launch server
+$ yarn build
+$ yarn start
+```
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+	<div class="w-full h-full flex">
+		<p class="m-auto">
+			Project running successfully
+		</p>
+	</div>
+</template>
+
+<script>
+export default {
+  	name: 'LandingPage',
+	layout: 'empty'
+}
+</script>
